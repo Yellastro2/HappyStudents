d5d801c99b007e1408e377d0c6eadfbe148037a9
Yellow Astronaut <77885272+Yellastro2@users.noreply.github.com>
2023-01-22 17:46:15 +0000

    all

--- HappyStudent_bot.py
+++ HappyStudent_bot.py
@@ -22,6 +22,10 @@ import mysql.connector
 from mysql.connector import (connection)
 from mysqlx import errorcode
 
+s_datafile = 'data.txt'
+k_userlist = 'user_list'
+s_datafold = 'usersdata/'
+
 host1 = 'ftp60.hostland.ru'
 host2 = '185.26.122.60'
 host3 = 'mysql60.hostland.ru'
@@ -37,6 +41,8 @@ config = {
 
 def get_quest_link(f_num, f_id):
   quests = f'http://happystudents.online/z{f_num}.php?id={f_id}'
+  if(f_num==-1):
+    return -1
   return quests
 
 
@@ -65,7 +71,7 @@ def select_db(f_select):
 
 
 def get_db_id(f_id):
-  f_sel_name = f"SELECT user_name,ref FROM users2 WHERE user_id = {f_id}"
+  f_sel_name = f"SELECT user_name,inviter FROM users2 WHERE user_id = {f_id}"
   result = select_db(f_sel_name)
   print(result)
   return result
@@ -78,40 +84,50 @@ def get_db_quest(f_id):
   f_sel = f"SELECT vz1,vz2,vz3,vz4,vz5 FROM users2 WHERE user_id = {f_id}"
   result = select_db(f_sel)
   print(result)
+  #result = get_quest_link(f_id,get_quest_num(result[0]))
+  return get_quest_num(result[0])
 
-  return result
 
-
-def get_quest_link(f_id, f_qz):
+def get_quest_num(f_qz):
   i = 1
   for q_qz in f_qz:
-    if (q_qz == 1):
+    if (q_qz == 0):
       return (i)
     i = i + 1
+  return -1
 
-
-my_secret = os.environ['tg_token']
-BOT_TOKEN = my_secret
+#my_secret = os.environ['tg_token']
+BOT_TOKEN = '5837317302:AAG1RQqheBdo1Bhv6th1LK9of-QDVjKKHWQ'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 lang_ru = {
   'txt_lang': '🇷🇺RUS🇷🇺',
-  'txt_input_id': 'Введите ID пользователя сайта',
+  'txt_input_id': 'Укажите свой ID от Бизнес воронки Happy Students',
   'txt_home': 'Здравствуйте, ',
-  'txt_make_compet': 'Выполните ваше задание:',
+  'txt_make_compet': 'Вы в данное время находитесь на этапе выполнения задания N. Чем быстрее выполните, тем быстрее у Вас не только создаётся автоматизированный, постоянно растущий бизнес, а также обеспечивается автоматический рост Вашего бизнеса. Выполнение задания не так сложно, как может Вам кажется.\n Приступайте выполнению: ',
+  'home_2': 'Если имеются какие проблемы, можете в любое время связаться с Вашим куратором:',
+  'kurator':'Куратор',
+  'admin':'Админ',
   'txt_set_compl': '✉️Отправить выполненное задание✉️',
   'input_comp': 'Отправьте выполненное задание сообщением сюда',
-  'sumbit_comp': 'Задание отправлено куратору'
+  'sumbit_comp': 'Задание отправлено куратору',
+  'error_ref_not_found': 'Реферал не присоединился к боту, невозможно отправить задание здесь',
+  'home_alldone': 'Вы выполнили все 5 задания. Поздравляем. У Вас уже образован постоянно, автоматические расширяющийся Бизнес с возможностью дополнительной массовой рекламы любого Вашего ресурса. Ваш бизнес уже будет расширяться и количество Ваших покупателей будут постоянно расти, даже если Вы уже больше ничего не будете делать. Но мы предоставляем Вам еще программные средства, если хотите еще более ускорить темп роста Вашего бизнеса.\n Пожалуйста, получайте:\n http://happystudents.online/programs.zip '
 }
 lang_uz = {
   'txt_lang': '🇺🇿UZB🇺🇿',
-  'txt_input_id': 'Enter ID of site accout',
-  'txt_home': 'Hello, ',
-  'txt_make_compet': 'Make your competition:',
-  'txt_set_compl': '✉️Send result✉️',
-  'input_comp': 'Send competiton as message here',
-  'sumbit_comp': 'Competition sendet to patron'
+  'txt_input_id': "Happy Students Business voronkasidagi o'z ID-ingizni kiriting",
+  'txt_home': 'Salom ',
+  'txt_make_compet': "Siz hozirda N-topshiriqni bajarish bosqichidasiz. Uni qanchalik tez bajarsangiz, shunchalik tez avtomatlashtirilgan, doimiy o‘sib borayotgan biznesni yaratibgina qolmay, balki biznesingizning avtomatik o‘sishini ham ta’minlaysiz. Vazifani bajarish siz o'ylagandek qiyin emas.\n Topshiriqni bajarishga kirishing:",
+  'home_2': "Agar sizda biron bir muammo bo'lsa, istalgan vaqtda kuratoringizga murojaat qilishingiz mumkin:",
+  'kurator':'Kurator',
+  'admin':'Admin',
+  'txt_set_compl': "✉️Topshiriqni jo'natish✉️",
+  'input_comp': "Topshiriq N bajarilganligi haqidagi ma'lumotni ushbu chatga yuboring",
+  'sumbit_comp': 'Kuratorzga yuborish!',
+  'error_ref_not_found': " Ishtirokchi botga ulanmagan. Ma'lumot yuborish imkoniyati yo'q.",
+  'home_alldone':"Siz barcha 5 ta vazifani bajardingiz. Tabriklaymiz. Siz allaqachon har qanday manbangizni qo'shimcha ommaviy reklama qilish imkoniyati bilan birga doimiy ravishda avtomatik ravishda kengayib borayotgan Biznesni yaratdingiz. Sizning biznesingiz doim  kengayib boradi va siz boshqa hech narsa qilmasangiz ham, mijozlaringiz soni doimiy ravishda o'sib boradi. Ammo biznesingiz rivojini yanada tezlashtirishni istasangiz, biz sizga ko'proq dasturiy vositalarni taqdim etamiz.\n Marhamat, qabul qiling:\n http://happystudents.online/programs.zip"
 }
 rus = 'rus'
 uzb = 'uzb'
@@ -123,6 +139,7 @@ form_router = Router()
 class Form(StatesGroup):
   ident = State()
   first_name = State()
+  ref = State()
   lang = State()
   like_bots = State()
   language = State()
@@ -135,7 +152,7 @@ class Form(StatesGroup):
 
 async def updateState(state, fkey, val, step):
   fchat_id = await state.get_data()
-  fchat_id = fchat_id['chat_id']
+  fchat_id = s_datafold+str(fchat_id['chat_id'])+'.json'
   try:
     f = open(fchat_id, 'r')
   except FileNotFoundError as e:
@@ -164,13 +181,43 @@ async def updateState(state, fkey, val, step):
       main_lang = lang_uz'''
   if (fkey == 'ident'):
     await state.update_data(ident=val)
+  if(fkey == 'first_name'):
+    await state.update_data(first_name=val)
+  if(fkey == 'ref'):
+    await state.update_data(ref=val)
   await state.set_state(step)
 
+async def get_chat_by_db_id(message,f_id):
+  fData = False
+  print('*'*10 +'\n GET CHAT BY ID '+str(f_id))
+  try:
+    f = open(s_datafile, 'r')
+    fData = json.load(f)
+  except (FileNotFoundError,ValueError) as e:
+    print(e)
+    print('id data error')
+    await message.answer('data file error')
+  finally:
+    f.close()
+  print('data readet')
+  print(fData)
+  if (fData):
+    for q_id in fData[k_userlist]:
+      if(q_id['user_id']==str(f_id)):
+        f_refid = {'name': q_id['tg_name'],
+                  'id': q_id['tg_id']}
+        return f_refid
+  return -1
 
-async def open_home(message, state):
+@form_router.message(Command(commands=["home"]))
+async def open_home(message, state)-> None:
   await state.set_state(Form.home)
   f_data = await state.get_data()
-  f_lang = f_data['lang']
+  try:
+    f_lang = f_data['lang']
+  except KeyError as e:
+    await command_start(message,state)
+    return
   kb = [
     [KeyboardButton(text=main_lang[f_lang]['txt_set_compl'])],
   ]
@@ -184,41 +231,82 @@ async def open_home(message, state):
   print('id what entered')
   print(id)
   f_name = f_data['first_name']
-
+  f_qwnum = get_db_quest(id)
+  f_qwforlink = f_qwnum
+  if(f_lang=='uzb'):
+    f_qwforlink = str(f_qwforlink)+'uz'
+  f_link = get_quest_link(f_qwforlink,id)
+  print(f_link)
+  print(message.from_user.id )
+  f_refid = f_data['ref']
+  f_curator = await get_chat_by_db_id(message,f_refid)
+  f_admin = await get_chat_by_db_id(message,1)
+  
+  if(f_curator==-1):
+    f_curator = 'not in Telegram'
+  else:
+    q_name = f_curator["name"]
+    q_id = f_curator["id"]
+    f_curator = f"<a href='tg://user?id={q_id}'>@{q_name}</a>"
+  if(f_admin==-1):
+    f_admin = 'not in Telegram'
+  else:
+    q_name = f_admin["name"]
+    q_id = f_admin["id"]
+    f_admin = f"<a href='tg://user?id={q_id}'>@{q_name}</a>"
+  if(f_qwnum==-1):
+    f_body = main_lang[f_lang]['home_alldone']
+  else:
+    
+    f_body = main_lang[f_lang]['txt_make_compet'].replace('N',str(f_qwnum))+'\n'+f_link+'\n'+main_lang[f_lang]['home_2']
   await message.answer(main_lang[f_lang]['txt_home'] + f_name + '\n' +
-                       main_lang[f_lang]['txt_make_compet'],
+                       f_body +'\n\n'+
+                       main_lang[f_lang]['kurator']+' - '+f_curator+'\n'+
+                       main_lang[f_lang]['admin']+' - '+f_admin,
+                       parse_mode = 'HTML',
                        reply_markup=keyboard)
 
 
 async def start_input_comp(message, state):
+  
   f_data = await state.get_data()
   f_lang = f_data['lang']
+  id = f_data['ident']
+  f_qwnum = get_db_quest(id)
   await state.set_state(Form.input_comp)
   print('start input competition')
-  await message.answer(main_lang[f_lang]['input_comp'])
+  f_body = main_lang[f_lang]['input_comp'].replace('N',str(f_qwnum))
+  await message.answer(f_body)
 
+  
 
+@form_router.message(Command(commands=["send_competition"]))
 @form_router.message(Form.home,
                      F.text.casefold() == lang_uz['txt_set_compl'].casefold())
-async def process_like_write_bots(message: Message, state: FSMContext) -> None:
-  await start_input_comp(message, state)
-
-
 @form_router.message(Form.home,
-                     F.text.casefold() == lang_ru['txt_set_compl'].casefold())
-async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+                     F.text.casefold()== lang_ru['txt_set_compl'].casefold())
+async def command_sendcomp(message: Message, state: FSMContext) -> None:
   await start_input_comp(message, state)
+
 
 
 @form_router.message(Form.input_comp)
-async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+async def send_competition(message: Message, state: FSMContext) -> None:
   f_data = await state.get_data()
   f_lang = f_data['lang']
   await state.set_state(Form.home)
   print('get competition:')
   print(message.text)
-  await message.answer(main_lang[f_lang]['sumbit_comp'])
+  resp = get_db_id(f_data['ref'])
 
+  f_chatid= await get_chat_by_db_id(message,f_data['ref'])
+  if(f_chatid==-1):
+    await message.answer(main_lang[f_lang]['error_ref_not_found'])
+    return
+  f_body = f_data['first_name'] + 'прислал(а) вам задание на проверку:\n '+ message.text
+  await bot.send_message(int(f_chatid['id']), f_body)
+  await message.answer(main_lang[f_lang]['sumbit_comp'])
+  return
 
 async def chose_lang(message, state):
   print('chat data empty')
@@ -235,16 +323,11 @@ async def chose_lang(message, state):
                                  resize_keyboard=True,
                                  input_field_placeholder="Выберите язык")
   await message.answer("Выбрать язык", reply_markup=keyboard)
-  # await message.answer("Hello!")
   return False
-
-
-s_datafile = 'data.txt'
-k_userlist = 'user_list'
-
 
 def save_data(f_data):
   fData = json.dumps(f_data)
+  print(fData)
   with open(s_datafile, "w") as my_file:
     my_file.write(fData)
     my_file.close()
@@ -254,35 +337,12 @@ def save_data(f_data):
 async def command_start(message: Message, state: FSMContext) -> None:
   print('start comand')
   fchat_id = message.chat.id
-  try:
-    f = open(s_datafile, 'r')
-  except FileNotFoundError as e:
-    f = open(s_datafile, 'x')
-    f = open(s_datafile, 'r')
-  fData = False
-  try:
-    fData = json.load(f)
-  except ValueError as e:
-    print('bot data still empty')
-  finally:
-    f.close()
-  #neef function from here
-  if (fData):
-    f_list = fData[k_userlist]
-    if (f_list.count(fchat_id) < 1):
-      f_list.append(fchat_id)
-      fData[k_userlist] = f_list
-      save_data(fData)
-  else:
-    fData = {k_userlist: [fchat_id]}
-    save_data(fData)
+  message.from_user.username
 
-
-#to here
-  datafilename = str(message.chat.id) + '.json'
+  datafilename = s_datafold+ str(message.chat.id) + '.json'
   print(message.chat.id)
 
-  await state.update_data(chat_id=datafilename)
+  await state.update_data(chat_id=message.chat.id)
   try:
     with open(datafilename, "r") as f:
       fDataStr = f.read()
@@ -300,6 +360,7 @@ async def command_start(message: Message, state: FSMContext) -> None:
     return
   try:
     await state.update_data(ident=fData['ident'])
+    await state.update_data(ref=fData['ref'])
   except (AttributeError, KeyError) as e:
     await state.set_state(Form.ident)
     await req_id(message, state)
@@ -324,7 +385,7 @@ async def req_id(message, state):
 
 @form_router.message(Form.lang,
                      F.text.casefold() == lang_ru['txt_lang'].casefold())
-async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+async def chose_rus_lang(message: Message, state: FSMContext) -> None:
   fLang = 'rus'
   print('lang ', fLang)
 
@@ -335,7 +396,7 @@ async def process_like_write_bots(message: Message, state: FSMContext) -> None:
 
 @form_router.message(Form.lang,
                      F.text.casefold() == lang_uz['txt_lang'].casefold())
-async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+async def chose_uzb_lang(message: Message, state: FSMContext) -> None:
   fLang = 'uzb'
   print('lang ', fLang)
   await updateState(state, 'lang', fLang, Form.ident)
@@ -357,14 +418,63 @@ async def process_read_id(message: Message, state: FSMContext) -> None:
   #f_name = f_name[2:-3]
   f_name = f_name[0]
   print(f_name)
-  f_finname = f_name[2:-6]
-  f_ref = f_name[-5:-3]
+  f_finname = f_name[0]
+  f_ref = f_name[1]
   print('write name')
   print(f_finname)
   print(f_ref)
   await updateState(state, 'ident', fId, Form.ident)
-  await updateState(state, 'first_name', f_name, Form.first_name)
+  await updateState(state, 'first_name', f_finname, Form.ident)
+  await updateState(state, 'ref', f_ref, Form.ident)
 
+  
+  try:
+    f = open(s_datafile, 'r')
+  except FileNotFoundError as e:
+    f = open(s_datafile, 'x')
+    f = open(s_datafile, 'r')
+  fData = False
+  try:
+    fData = json.load(f)
+  except ValueError as e:
+    print('bot data still empty')
+  finally:
+    f.close()
+  fchat_id = message.chat.id
+  f_tgid = message.from_user.id
+  f_username = message.from_user.username
+  if (fData):
+    f_list = fData[k_userlist]
+    isusercontain = False
+    is_changed = False
+    for q_user in f_list:
+      if(q_user['user_id']==fId): 
+        if(q_user['chat_id']!=fchat_id):
+          q_user['chat_id']=fchat_id
+          is_changed=True
+        if(q_user['tg_name']!=f_username):
+          q_user['tg_name']=f_username
+          is_changed=True
+        if(q_user['tg_id']!=f_tgid):
+          q_user['tg_id']=f_tgid
+          is_changed=True
+        isusercontain = True
+    if(not isusercontain):
+      f_list.append({'chat_id' :fchat_id,
+                         'user_id' : fId,
+                    'tg_name': f_username,
+                    'tg_id': f_tgid})
+      is_changed=True
+    if(is_changed):
+      fData[k_userlist] = f_list
+      save_data(fData)
+  else:
+    fData = {k_userlist:[{'chat_id' :fchat_id,
+                         'user_id' : fId,
+                         'tg_name': f_username,
+                         'tg_id': f_tgid}]}
+    save_data(fData)
+  
   await open_home(message, state)
 
 
@@ -401,6 +511,12 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     return
   # print(state.lang)
   logging.info("Cancelling state %r", current_state)
+  fchat_id = s_datafold+ str(data['chat_id'])+'.json'
+  if os.path.exists(fchat_id):
+        os.remove(fchat_id)
+  else:
+        print("The file does not exist")
+    
   await state.clear()
   await message.answer(
     "Cancelled.",
@@ -445,7 +561,7 @@ async def process_admin_enter(message: Message, state: FSMContext) -> None:
 async def process_post(message: Message, state: FSMContext) -> None:
   print('print entered: ' + message.text)
   try:
-    f_file = open('data.txt', 'r')
+    f_file = open(s_datafile, 'r')
     f_data = json.load(f_file)
   except (FileExistsError, ValueError) as e:
     await bot.send_message(message.from_user.id,
